5c17c5caea3d1fb1869a4f146ef86beff681183c
soltsega <tsegasolomon538@gmail.com>
2026-03-10 23:01:43 +0300

    Daily commit- Java

--- methods/RandomNumGenerator.java
+++ methods/RandomNumGenerator.java
@@ -2,8 +2,18 @@ package methods;
 import java.util.Random;
 // Methods in java
 // Now, we will see the randomm number generator method in java
+
+// the fill() method of an array
+// it is used to fill an array with a specific item
+// it is usd like this
+
+// Arrays.fill(arrayName, "The value we wanted to fill with")
+
+
+// varargs in java are used to pack arguments in an array
+// it is like this one 
+// public int classname (datatype of the collection... the name of the array we want to collect in)
 public class RandomNumGenerator {
-    // 
     Random random = new Random();
     
     public void generateRandomNumbers() {
